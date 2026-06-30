e3b6cda5d04170c4ecdefa04b8d6bba7b8bf8c93
Kasznár András <celestial@Kasznar-MacBook-Pro.local>
2026-06-30 21:16:41 +0200

    Use Rent-Is-Due deploy pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const repositoryName =
+  process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'skilltree'
+
 export default defineConfig({
-  base: './',
+  base: process.env.GITHUB_ACTIONS ? `/${repositoryName}/` : '/',
   plugins: [react()],
 })
